a0f7827832854cb2e24056b09773ed6b9083ffb3
William Wang <wow7@cornell.edu>
2019-06-10 02:29:26 -0400

    basic router/boilerplate setup

---
+++ frontend/src/pages/Browse.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Browse extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Browse;
