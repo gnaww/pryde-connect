b99de88ee9b9bd43b7d8e3f3dc03e308140a6dc6
William Wang <wow7@cornell.edu>
2019-06-26 16:50:39 -0400

    rename css file

--- frontend/src/components/ProfilePictureModal.js
+++ frontend/src/components/ProfilePictureModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from '../styles/Modal.module.css';
+import styles from '../styles/ProfilePictureModal.module.css';
 
 class ProfilePictureModal extends Component {
     constructor(props) {
