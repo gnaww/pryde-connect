bb0410e41d93bd0f2a7cdc57415d5bf8b2fcfe43
Barron DuBois <bfd27@cornell.edu>
2019-06-27 11:12:10 -0400

    more of homepage (css still needs some work)

--- frontend/src/pages/Index.js
+++ frontend/src/pages/Index.js
@@ -2,128 +2,19 @@ import React, { Component } from 'react';
 import styles from '../styles/Index.module.css';
 import { Link } from 'react-router-dom';
 import logo from "../images/pryde-symbol.png";
-
+import logoWhite from '../images/pryde-white.png';
+import connect2 from '../images/connect2.png'
+import screenshot1 from '../images/screenshot1.png'
+import screenshot2 from '../images/screenshot2.png'
+import screenshot3 from '../images/screenshot3.png'
 class Login extends Component {
 
     render() {
         return (
             <>
-                {/*    <div className={styles.welcomePic}>*/}
-                {/*        <img src={logo} alt="PRYDE logo" className={styles.imagelogo} />*/}
-                {/*        /!*<h1 className={styles.topText}> pic goes here</h1>*!/*/}
-
-                {/*    </div>*/}
-
-                {/*    <div className={styles.welcomeText}>*/}
-
-                {/*        <h1 className={styles.topText}>*/}
-                {/*            Welcome to PRYDE’s Research Connector!*/}
-                {/*        </h1>*/}
-                {/*        <br></br>*/}
-                {/*        <input className={styles.loginButton} type="submit" value="SIGN IN TO CONTINUE" />*/}
-
-                {/*    </div>*/}
-
-
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-
-
-                {/*<div className={styles.aboutDiv}>*/}
-                {/*    <h1 className={styles.aboutCaps}>*/}
-                {/*        ABOUT*/}
-                {/*    </h1>*/}
-
-
-                {/*    <h1 className={styles.topText}>*/}
-
-                {/*        PRYDE created this space to connect researchers and practitioners with a shared interest in youth development.*/}
-                {/*        <br></br>*/}
-                {/*        <br></br>*/}
-                {/*    </h1>*/}
-
-                {/*</div>*/}
-
-                {/*<div className={styles.aboutPic}>*/}
-                {/*    <img src={logo} alt="PRYDE logo" className={styles.imagelogo} />*/}
-
-                {/*</div>*/}
-
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-                {/*<br></br>*/}
-
-
-
-
-                {/*<div className={styles.homeDiv1}>*/}
-
-                {/*    <h1 className={styles.cardtext}>*/}
-                {/*        •    Are you looking to collaborate on a research idea? Search our network of profiles for people who share your research interest and reach out to them.*/}
-                {/*        <br></br>*/}
-                {/*        <br></br>*/}
-                {/*    </h1>*/}
-
-
-                {/*</div>*/}
-
-
-
-
-                {/*<div className={styles.homeDiv2}>*/}
-                {/*    <h1 className={styles.cardtext}>*/}
-
-
-                {/*    •    Do you want to be part of a study that’s already in progress? Check out our list of research opportunities and get involved.*/}
-                {/*        <br></br>*/}
-                {/*        <br></br>*/}
-                {/*    </h1>*/}
-
-                {/*</div>*/}
-
-
-                {/*<div className={styles.homeDiv3}>*/}
-
-                {/*<h1 className={styles.cardtext}>*/}
-
-                {/*•    Do you want to help PRYDE identify overlapping research interests and needs? Create a profile and tell us about the work you do and the projects you want to see.*/}
-                {/*    Create your profile and let’s start making connections!*/}
-                {/*    <br></br>*/}
-                {/*    <br></br>*/}
-                {/*</h1>*/}
-
-
-                {/*</div>*/}
-
 
 
                 <div id={styles.div1}>
-
 
                     <p id={styles.graphictext}>
 
@@ -135,21 +26,10 @@ class Login extends Component {
 
                     </h1>
 
-
-
-
                 </div>
 
 
                 <div id={styles.div2}>
-
-
-
-
-
-
-
-
 
 
                     <h1 id={styles.aboutheader}> About [Website Name] </h1>
@@ -158,19 +38,14 @@ class Login extends Component {
                         [extended blurb about website] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris.
                     </p>
 
-
-
-
-
-
-
-
+                    <img className={styles.clocktower} src={connect2} alt="Cornell ClockTower" />
 
                 </div>
 
+
+
+
                 <div id={styles.div3}>
-
-
 
                     <h1 id={styles.communicate}> Communicate across the state </h1>
 
@@ -187,6 +62,11 @@ class Login extends Component {
                     <h1 id={styles.understand}> Understand the needs of the New York youth  </h1>
 
                     <p id={styles.feedback}> Get feedback from 4-H practitioners across New York State about what issues they see in their communities </p>
+
+
+                    <img className={styles.screenshot1} src={screenshot1} alt="Screenshot 1" />
+                    <img className={styles.screenshot2} src={screenshot2} alt="Screenshot 2" />
+                    <img className={styles.screenshot3} src={screenshot3} alt="Screenshot 3" />
 
 
 
