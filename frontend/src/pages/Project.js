bad28cf1439b1bfdd14eeff529922934b42aaf52
William Wang <wow7@cornell.edu>
2019-06-27 19:57:42 -0400

    finished project page styling

--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -7,47 +7,54 @@ class Project extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            project: {
-                id: 1,
-                name: "Project Name",
-                owner: {
-                    name: "John Smith",
-                    affiliation: "Org Name",
-                    location: "Ithaca, NY",
-                    email: "something@something.edu",
-                    phone: "1234567890",
-                    website: "https://something.com"
+            id: 1,
+            name: "Project Name",
+            owner: {
+                name: "John Smith",
+                affiliation: "Org Name",
+                location: "Ithaca, NY",
+                email: "something@something.edu",
+                phone: "1234567890",
+                website: "https://something.com"
+            },
+            status: "completed",
+            summary: "[this is where the goal question comes in] details or summary of project or research partner] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris.",
+            stats: {
+                researchTopics: ["Positive Youth Development", "Civic Engagement"],
+                ageRange: ["15-17 years old"],
+                deliveryModes: ["Afterschool Programs"],
+                timeline: "Next summer",
+                commitmentLength: "1 year",
+                incentives: ["Educators", "Participants"]
+            },
+            collaborators: [
+                {
+                    id: 1,
+                    type: "partner",
+                    name: "Mary Jane",
+                    role: "Practitioner",
+                    affiliation: "Cornell"
                 },
-                status: "completed",
-                summary: "[this is where the goal question comes in] details or summary of project or research partner] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris.",
-                stats: {
-                    researchTopics: ["Positive Youth Development", "Civic Engagement"],
-                    ageRange: ["15-17 years old"],
-                    deliveryModes: ["Afterschool Programs"],
-                    timeline: "Next summer",
-                    commitmentLength: "1 year",
-                    incentives: ["Educators", "Participants"]
-                },
-                collaborators: [
+                {
+                    id: 2,
+                    type: "partner",
+                    name: "Bill Nye",
+                    role: "Researcher",
+                    affiliation: "Cornell"
+                }
+            ],
+            additionalInformation: {
+                text: "[additional information] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris.",
+                files: [
                     {
-                        id: 1,
-                        type: "partner",
-                        name: "Mary Jane",
-                        role: "Practitioner",
-                        affiliation: "Cornell"
+                        filename: "results.pdf",
+                        source: "somelinkhere"
                     },
                     {
-                        id: 2,
-                        type: "partner",
-                        name: "Bill Nye",
-                        role: "Researcher",
-                        affiliation: "Cornell"
+                        filename: "stats.pdf",
+                        source: "somelinkhere"
                     }
-                ],
-                additionalInformation: {
-                    text: "[additional information] Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget neque in mauris tristique condimentum a quis mauris.",
-                    filename: "results.pdf"
-                }
+                ]
             }
         };
     }
@@ -56,89 +63,104 @@ class Project extends Component {
     }
 
     render() {
-        const { project } = this.state;
+        const { name, owner, status, summary, stats, collaborators, additionalInformation } = this.state;
+
+        let statusFormatted = '';
+        if (status === "completed") {
+            statusFormatted = "COMPLETE"
+        } else if (status === "not-started") {
+            statusFormatted = "NOT STARTED";
+        } else if (status === "in-progress") {
+            statusFormatted = "IN PROGRESS"
+        }
 
         return (
             <div className={styles.container}>
-                <main>
-                    <button className={styles.editButton}>
-                        <img src={editButtonOrange} alt="Edit button" />
-                    </button>
-                    <header>
+                <main className={styles.projectWrapper}>
+                    <div className={styles.editButtonWrapper}>
+                        <button className={styles.editButton}>
+                            <img src={editButtonOrange} alt="Edit button" />
+                        </button>
+                    </div>
+                    <header className={styles.projectHeader}>
                         <div>
-                            <h1>{project.name}</h1>
-                            <h2>{`${project.owner.name} - ${project.owner.affiliation}`}</h2>
-                            <h2>{project.location}</h2>
+                            <h1>{name}</h1>
+                            <h2>{`${owner.name} - ${owner.affiliation}`}</h2>
+                            <h2>{owner.location}</h2>
                         </div>
-                        <div>
-                            <h3>{project.status}</h3>
+                        <div className={styles.projectContact}>
+                            <h3>{statusFormatted}</h3>
                             <ul>
                                 <li>
-                                    <a href={`mailto:${project.owner.email}`}>{project.owner.email}</a>
+                                    <a href={`mailto:${owner.email}`}>{owner.email}</a>
                                 </li>
                                 <li>
-                                    <a href={`tel:${project.owner.phone}`}>({project.owner.phone.slice(0, 3)})-{project.owner.phone.slice(3, 6)}-{project.owner.phone.slice(6, 10)}</a>
+                                    <a href={`tel:${owner.phone}`}>({owner.phone.slice(0, 3)})-{owner.phone.slice(3, 6)}-{owner.phone.slice(6, 10)}</a>
                                 </li>
                                 <li>
-                                    <a href={project.owner.website} target="_blank" rel="noopener noreferrer">{project.owner.website.replace(/(^\w+:|^)\/\//, '')}</a>
+                                    <a href={owner.website} target="_blank" rel="noopener noreferrer">{owner.website.replace(/(^\w+:|^)\/\//, '')}</a>
                                 </li>
                             </ul>
                         </div>
                     </header>
-                    <section>
-                        <h2>SUMMARY</h2>
-                        <p>{project.summary}</p>
+                    <section className={styles.summary}>
+                        <h2 className={styles.sectionHeader}>SUMMARY</h2>
+                        <p>{summary}</p>
                     </section>
-                    <div>
-                        <section>
-                            <h2>STATS</h2>
+                    <div className={styles.projectInformationWrapper}>
+                        <section className={styles.stats}>
+                            <h2 className={styles.sectionHeader}>STATS</h2>
                             <div>
                                 <h3>Research Topics</h3>
                                 <ul>
                                     {
-                                        project.stats.researchTopics.map((interest, idx) => <li key={idx}>{interest}</li>)
+                                        stats.researchTopics.map((interest, idx) => <li key={idx}>{interest}</li>)
                                     }
                                 </ul>
                                 <h3>Age Range</h3>
                                 <ul>
                                     {
-                                        project.stats.ageRange.map((interest, idx) => <li key={idx}>{interest}</li>)
+                                        stats.ageRange.map((interest, idx) => <li key={idx}>{interest}</li>)
                                     }
                                 </ul>
                                 <h3>Delivery Modes</h3>
                                 <ul>
                                     {
-                                        project.stats.deliveryModes.map((interest, idx) => <li key={idx}>{interest}</li>)
+                                        stats.deliveryModes.map((interest, idx) => <li key={idx}>{interest}</li>)
                                     }
                                 </ul>
                                 <h3>Timeline</h3>
-                                <p>{project.stats.timeline}</p>
-                                <h3>Length of Participant Commitment</h3>
-                                <p>{project.stats.commitmentLength}</p>
+                                <p>{stats.timeline}</p>
+                                <h3>Participant Commitment Length</h3>
+                                <p>{stats.commitmentLength}</p>
                                 <h3>Benefits + Incentives</h3>
                                 <ul>
                                     {
-                                        project.stats.incentives.map((interest, idx) => <li key={idx}>{interest}</li>)
+                                        stats.incentives.map((interest, idx) => <li key={idx}>{interest}</li>)
                                     }
                                 </ul>
                             </div>
                         </section>
-                        <section>
-                            <h2>COLLABORATORS</h2>
-                            <div>
+                        <section className={styles.collaborators}>
+                            <h2 className={styles.sectionHeader}>COLLABORATORS</h2>
+                            <div className={styles.collaboratorsWrapper}>
                                 {
-                                    project.collaborators.map(collaborator => <SearchResult {...collaborator} />)
+                                    collaborators.map(collaborator => <SearchResult {...collaborator} />)
                                 }
                             </div>
                         </section>
                     </div>
-                    <section>
-                        <h2>ADDITIONAL INFORMATION</h2>
+                    <section className={styles.additionalInformation}>
+                        <h2 className={styles.sectionHeader}>ADDITIONAL INFORMATION</h2>
                         <div>
-                            <p>{project.additionalInformation.text}</p>
-                            <button>
-                                {project.additionalInformation.filename}
-                            </button>
+                            <p>{additionalInformation.text}</p>
+                            {
+                                additionalInformation.files.map((file, idx) => 
+                                    <a key={idx} href={file.source}>
+                                        {file.filename}
+                                    </a>
+                                )
+                            }
                         </div>
                     </section>
                 </main>
