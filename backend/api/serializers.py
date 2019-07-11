db95ef484a93d1d69f60530a635fb014a4657973
William Wang <wow7@cornell.edu>
2019-07-11 03:31:26 -0400

    updated serializers to match updated models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -7,27 +7,25 @@ from .models import PUser, Project
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('pk', 'user', 'name_of_study', 'collaborators', 'status', 'research_topics',
-                  'age_youth', 'goal', 'timeline', 'participant_involvement', 'incentives',
-                  'incentives_participants', 'delivery_models', 'additional_desc', 'website')
+        fields = '__all__'
 
-# Used for the project cards in browse page
+# Used for the project cards in the browse page
 class ProjectShortSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        fields = ('pk', 'type', 'name', 'owner', 'status', 'summary')
+        fields = ('pk', 'name', 'owner', 'status', 'summary')
 
 class UserSerializer(serializers.ModelSerializer):
     projects = ProjectSerializer(many=True, read_only=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('pk', 'first_name', 'last_name', 'email', 'projects', )
+        fields = '__all__'
 
-# Used for the user cards in browse page
+# Used for the user cards in the browse page
 class UserShortSerializer(serializers.ModelSerializer):
     projects = ProjectSerializer(many=True, read_only=True)
 
     class Meta:
         model = get_user_model()
-        fields = ('pk', 'type', 'first_name', 'last_name', 'role', 'affiliation')
+        fields = ('pk', 'firstName', 'lastName', 'role', 'affiliation')
