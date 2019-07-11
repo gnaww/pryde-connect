7fb0ea51d5060622b6c8f7cfab4bc4827acf9765
William Wang <wow7@cornell.edu>
2019-07-11 03:30:24 -0400

    cleanup and renaming endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,23 +1,14 @@
 from django.urls import path, include
 from django.conf.urls import url
-
-
-from .views import hello_world, ProjectList, ProjectCreate, ProjectView, UserView, ProjectViewOne, SingleUser
+from .views import ProjectList, ProjectCreate, ProjectView, UserView, UserProjectList, SingleUser
 
 # All of these endpoints begin with 'api/v1/'
 urlpatterns = [
-
-    path('hello/', hello_world),
-
     path('project/', ProjectList.as_view()),
-
     path('project/create/', ProjectCreate.as_view()),
     path('project/<int:pk>/', ProjectView.as_view()),
+
     # path('user/<int:pk>/', SingleUser.as_view()),
-
-    path('project/user/<int:pk>/', ProjectViewOne.as_view()),
     path('user/<int:pk>/', UserView.as_view()),
-
-
-
+    path('user/<int:pk>/projects', UserProjectList.as_view()),
 ]
