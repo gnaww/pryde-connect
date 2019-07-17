4ef0cddf359b23c5d3b6b1f0e04964560586ad40
Barron <bfd27@cornell.edu>
2019-07-17 15:09:40 -0400

    finished insert user script... open new terminal and run it while server is running

--- backend/scripts/insert_users.py
+++ backend/scripts/insert_users.py
@@ -23,29 +23,239 @@ class User:
         self.researchNeeds = researchNeeds
         self.evaluationNeeds = evaluationNeeds
 
+
 def pump_into_servers():
-    #
-    # user1 = User("role", "display role", "affiliation", "location", "email", "phone", "website",
-    #              "research interests", "research description", "roles", "age ranges",
-    #              "youth program types", "delivery modes", "research Needs", "evaluation needs")
-
-
-    user1 = {
-        'email': 'test@gmail.com',
-        'password1': 'Kimber3915',
-        'password2': 'Kimber3915',
-        'first_name': 'test',
-        'last_name': 'user'
-    }
-
-    url = 'https://localhost:8000/api/v1/rest-auth/registration/'
+    url = 'http://localhost:8000/api/v1/rest-auth/registration/'
     headers = {
         'Content-Type': 'application/json',
         'cache-control': 'no-cache'
     }
 
-    response = requests.request("POST", url, data=json.dumps(user1), headers=headers)
-    print(response)
+    user1 = {
+        "email": "test1111@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user1",
+        "role": 1,
+        "displayRole": 3,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
 
-# pump_into_servers()
+    user2 = {
+        "email": "test22222@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user1",
+        "role": 1,
+        "displayRole": 3,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user3 = {
+        "email": "test33333@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 1,
+        "displayRole": 2,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user4 = {
+        "email": "test44444@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user5 = {
+        "email": "test55555@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user6 = {
+        "email": "test6666@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user7 = {
+        "email": "test777@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user8 = {
+        "email": "test8888@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user9 = {
+        "email": "test9999@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    user10 = {
+        "email": "test101010@gmail.com",
+        "password1": "Kimber3915",
+        "password2": "Kimber3915",
+        "first_name": "test",
+        "last_name": "user",
+        "role": 2,
+        "displayRole": 6,
+        "affiliation": "cornell university",
+        "location": "ithaca, NY",
+        "phonenumber": "+144322322438",
+        "website": "http://www.google.com",
+        "researchDescription": "i love doing research",
+        "roles": ["string1", "string2", "string3"],
+        "ageRanges": ["3-5", "7-10"],
+        "researchInterests": ["ahsdfkl", "alsdkf"],
+        "youthProgramTypes": ["string1", "string2"],
+        "deliveryModes": ["laskjfd", "aldskjf"],
+        "researchNeeds": ["asldkfjsdlkf", "alsdkjfs"],
+        "evaluationNeeds": ["alkdsjfldk", "alsdkfjlksd"]
+    }
+
+    users = [user1, user2, user3, user4, user5, user6, user7, user8, user9, user10]
+
+    for user in users:
+        response = requests.request("POST", url, data=json.dumps(user), headers=headers)
+        print(response)
+
+pump_into_servers()
 
