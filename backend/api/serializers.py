1a4c2c64aa171f5a8f216336715886c89d97d7dd
Barron <bfd27@cornell.edu>
2019-07-17 17:47:05 -0400

    added user to projects

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,10 +3,23 @@ from django.contrib.auth import get_user_model
 from .models import PUser, Project
 
 
+class MiniUserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PUser
+        fields = ('pk', 'first_name', 'last_name', 'affiliation', 'location', 'email', 'phone', 'website')
+
+
 class ProjectSerializer(serializers.ModelSerializer):
+    status = serializers.SerializerMethodField()
+    owner = MiniUserSerializer(many=False, read_only=True)
+
     class Meta:
         model = Project
         fields = '__all__'
+
+    def get_status(self, obj):
+        return obj.get_status_display()
 
 # Used for the project cards in the browse page
 class ProjectShortSerializer(serializers.ModelSerializer):
