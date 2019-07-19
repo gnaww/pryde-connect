40acab07e5421883f9b2a8b6235d8545142a55c7
William Wang <wow7@cornell.edu>
2019-07-19 04:06:28 -0400

    improved aesthetics

--- frontend/src/pages/CreateProfile/ReviewFinish.js
+++ frontend/src/pages/CreateProfile/ReviewFinish.js
@@ -1,63 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../styles/CreateProfile.module.css';
-import ProfileButton from '../../images/profile.svg';
-import HomeButton from '../../images/home.svg';
-import StudiesButton from '../../images/studies.svg';
+import ProfileButton from '../../images/profile.png';
+import HomeButton from '../../images/home.png';
+import StudiesButton from '../../images/studies.png';
 
 const navigationOptions = [
     {
         img: ProfileButton,
-        unclicked: styles.profileNavImg,
-        link: "/"
+        styling: styles.profileNavImg,
+        link: "/myprofile",
+        text: "VIEW YOUR PROFILE"
     },
     {
         img: HomeButton,
-        unclicked: styles.homeNavImg,
-        link: "/"
+        styling: styles.homeNavImg,
+        link: "/",
+        text: "BACK TO HOME"
     },
     {
         img: StudiesButton,
-        unclicked: styles.studiesNavImg,
-        link: "/browse"
+        styling: styles.studiesNavImg,
+        link: "/browse",
+        text: "BROWSE STUDIES"
     }
 
 ];
 
-class ReviewFinish extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clicked: null
-        };
-    }
-
-    handleClick = (index) => () => {
-        this.setState({ clicked: index });
-    }
-
-    render() {
-        return (
-            <div className={styles.finishPage}>
-                {
-                    navigationOptions.map((nav, index) => {
-                        return (
-                            <div className={styles.roleCard} key={index}>
-                                <Link to={nav.link}>
-                                    <img
-                                        className={nav.unclicked}
-                                        src={nav.img}
-                                        onClick={this.handleClick(index)}
-                                        alt={"Go to another page."}
-                                    />
-                                </Link>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
-}
+const ReviewFinish = () => (
+    <div className={styles.finishPage}>
+        {
+            navigationOptions.map((nav, index) => {
+                return (
+                    <div className={styles.roleCard} key={index}>
+                        <Link className={styles.roleLink} to={nav.link}>
+                            <img
+                                className={nav.styling}
+                                src={nav.img}
+                                alt="Go to another page."
+                            />
+                            <p>{nav.text}</p>
+                        </Link>
+                    </div>
+                );
+            })
+        }
+    </div>
+);
 
 export default ReviewFinish;
