1e9a44047552e1820f96f605ea664b50863372cc
William Wang <wow7@cornell.edu>
2019-07-19 04:03:44 -0400

    rename variables to match backend, fix validation, add question to form

--- frontend/src/pages/CreateProfile/PractitionerQuestions.js
+++ frontend/src/pages/CreateProfile/PractitionerQuestions.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
 import styles from '../../styles/CreateProfile.module.css';
-import { getCheckboxQuestion, getDropDownQuestion, getCheckedValuesArray } from './QAComponents';
-import { PractitionerInformation, practitionerQAForm } from './Constants';
+import { getCheckboxQuestion, getDropDownQuestion, getCheckedValuesArray, getPractitionerRoleQuestion } from './QAComponents';
+import { PractitionerInformation, practitionerQAForm } from './FormContent';
 
 class PractitionerQuestions extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            locatedAtCornell: null,
-            location: {
-                institution: "",
-                address: ""
+            locatedAtCCE: null,
+            affiliation: "",
+            location: "",
+            displayRole: {
+                option: "",
+                other: ""
             },
-            roleDescriptions: getCheckedValuesArray(PractitionerInformation.RoleDescriptions),
-            ageGroups: getCheckedValuesArray(PractitionerInformation.AgeGroups),
-            deliveryTypes: getCheckedValuesArray(PractitionerInformation.ProgramDeliveryModels),
-            researchTopics: getCheckedValuesArray(PractitionerInformation.ResearchTopics)
-        }
+            roles: getCheckedValuesArray(PractitionerInformation.RoleDescriptions),
+            ageRanges: getCheckedValuesArray(PractitionerInformation.AgeGroups),
+            deliveryModes: getCheckedValuesArray(PractitionerInformation.ProgramDeliveryModels),
+            researchInterests: getCheckedValuesArray(PractitionerInformation.ResearchTopics)
+        };
     }
 
     componentDidUpdate(_prevProps, _prevState) {
         function keyIsValid(state, key) {
             let last = state[key].length - 1;
-            return state[key].filter(r => r.checked).length > 0
-                && (!state[key][last].checked || state[key][last].other !== "");
+            if (key === "ageRanges") {
+                return state[key].filter(r => r.checked).length > 0;
+            }
+            else if (key === "displayRole") {
+                if (state[key].option === "Other: ") {
+                    return state[key].other !== "";
+                } else {
+                    return state[key].option !== "";
+                }
+            } else {
+                return state[key].filter(r => r.checked).length > 0
+                    && (!state[key][last].checked || state[key][last].other !== "");
+            }
         }
 
         function isInvalid(state) {
-            let locationValid = state.location.institution !== "" && state.location.address !== "";
-            let keys = ["roleDescriptions", "ageGroups", "deliveryTypes", "researchTopics"];
+            let locationValid = state.affiliation !== "" && state.location !== "";
+            let keys = ["displayRole", "roles", "ageRanges", "deliveryModes", "researchInterests"];
             let validArray = keys.map(k => keyIsValid(state, k));
             return !locationValid || validArray.filter(v => !v).length !== 0;
         }
@@ -37,8 +50,6 @@ class PractitionerQuestions extends Component {
             let error = isInvalid(this.state);
             this.props.onSubmitData(this.state, error);
         }
-
-        return null;
     }
 
     componentDidMount() {
@@ -47,37 +58,49 @@ class PractitionerQuestions extends Component {
         }
     }
 
-    setLocatedAtCornell = event => {
+    setLocatedAtCCE = event => {
         this.setState({
-            locatedAtCornell: event.target.value,
-            location: {
-                institution: "",
-                address: ""
+            locatedAtCCE: event.target.value === "true",
+            affiliation: "",
+            location: ""
+        });
+    }
+
+    setDisplayRole = event => {
+        this.setState({
+            displayRole: {
+                other: "",
+                option: event.target.value
             }
         });
     }
 
     setLocationDropdown = event => {
         this.setState({
-            location: {
-                institution: "Cornell Cooperative Extension",
-                address: event.target.value
-            }
+            affiliation: "Cornell Cooperative Extension",
+            location: event.target.value
         });
     }
 
     setLocationTextbox = key => event => {
         let value = event.target.value;
-        this.setState((prevState, _props) => ({
-            location: {
-                ...prevState.location,
-                [key]: value
-            }
-        }));
+
+        if (key === "affiliation") {
+            this.setState((prevState, _props) => ({
+                affiliation: value,
+                location: prevState.location
+            }));
+        } else {
+            this.setState((prevState, _props) => ({
+                affiliation: prevState.affiliation,
+                location: value
+            }));
+        }
     }
 
     setValues = (key, index, text) => {
         let copy = Array.from(this.state[key]);
+
         if (text !== null) {
             copy[index].other = text;
         }
@@ -85,44 +108,45 @@ class PractitionerQuestions extends Component {
             copy[index].checked = !copy[index].checked;
             copy[index].other = "";
         }
+
         this.setState({
             [key]: copy
         });
     }
 
     getQAComponent = (qa, index) => {
+        const defaultLocatedAtCCE = this.state.locatedAtCCE !== null ? this.state.locatedAtCCE : "";
+        const defaultCounty = this.state.location ? this.state.location : "";
+
         return (
             <li className={styles.numberedList} key={index}>
-                {getDropDownQuestion(qa, this.setLocatedAtCornell)}
-                {getCheckboxQuestion(qa, this.setValues, this.state)}
+                { getDropDownQuestion(qa, this.setLocatedAtCCE, defaultLocatedAtCCE) }
+                { getCheckboxQuestion(qa, this.setValues, this.state) }
+                { getPractitionerRoleQuestion(qa, this.setDisplayRole, this.state)}
                 {
-                    qa.id === 0 && this.state.locatedAtCornell !== null &&
+                    qa.id === 0 && this.state.locatedAtCCE !== null &&
                     (
                         <div className={styles.form}>
                             {
-                                this.state.locatedAtCornell === "true" &&
-                                getDropDownQuestion(qa.extra, this.setLocationDropdown)
-                            }
-                            {
-                                this.state.locatedAtCornell === "false" &&
-                                (
-                                    <>
-                                        <input
-                                            className={styles.longTextInput}
-                                            placeholder="What is your institution or organization?"
-                                            type="text"
-                                            value={this.state.location.institution}
-                                            onChange={this.setLocationTextbox("institution")}
-                                        />
-                                        <input
-                                            className={styles.longTextInput}
-                                            placeholder="Where are you located?"
-                                            type="text"
-                                            value={this.state.location.address}
-                                            onChange={this.setLocationTextbox("address")}
-                                        />
-                                    </>
-                                )
+                                this.state.locatedAtCCE ?
+                                getDropDownQuestion(qa.extra, this.setLocationDropdown, defaultCounty)
+                                :
+                                <>
+                                    <input
+                                        className={styles.longTextInput}
+                                        placeholder="What is your institution or organization?"
+                                        type="text"
+                                        value={this.state.affiliation}
+                                        onChange={this.setLocationTextbox("affiliation")}
+                                    />
+                                    <input
+                                        className={styles.longTextInput}
+                                        placeholder="Where are you located?"
+                                        type="text"
+                                        value={this.state.location}
+                                        onChange={this.setLocationTextbox("location")}
+                                    />
+                                </>
                             }
                         </div>
                     )
@@ -142,7 +166,7 @@ class PractitionerQuestions extends Component {
                     }
                 </ol>
             </div>
-        )
+        );
     }
 }
 
