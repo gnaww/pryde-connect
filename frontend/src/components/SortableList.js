ede7f8ac7f40d1f30a6edf94cf46143c559e4332
William Wang <wow7@cornell.edu>
2019-07-25 03:17:10 -0400

    implement remaining sorting options

--- frontend/src/components/SortableList.js
+++ frontend/src/components/SortableList.js
@@ -1,18 +1,65 @@
 import React from 'react';
 import SearchResult from './SearchResult';
 
-const nameCompareAsc = (a, b) => {
-    if (a.type === "project" ) {
-        return a.name.localeCompare(b.name, 'en');
-    } else {
+const stringCompareAsc = stringType => (a, b) => {
+    if (stringType === "name" && a.type === "partner")  {
         const nameA = `${a.first_name} ${a.last_name}`;
         const nameB = `${b.first_name} ${b.last_name}`;
-        return nameA.localeCompare(nameB, 'en');
+        return nameA.localeCompare(nameB, "en");
+    } else if (stringType === "name" && a.type === "project") {
+        const nameA = `${a.owner.first_name} ${a.owner.last_name}`;
+        const nameB = `${b.owner.first_name} ${b.owner.last_name}`;
+        return nameA.localeCompare(nameB, "en");
+    } else if (stringType === "location" && a.type === "partner") {
+        return a.location.localeCompare(b.location, "en");
+    } else if (stringType === "location" && a.type === "project") {
+        return a.owner.location.localeCompare(b.owner.location, "en");
     }
 };
 
-const nameCompareDesc = (a, b) => {
-    return -nameCompareAsc(a, b);
+const stringCompareDesc = stringType => (a, b) => {
+    return -stringCompareAsc(stringType)(a, b);
+};
+
+const numProjectsCompareAsc = (a, b) => {
+    return a.numProjects - b.numProjects;
+};
+
+const numProjectsCompareDesc = (a, b) => {
+    return b.numProjects - a.numProjects;
+};
+
+const dateCompareAsc = (a, b) => {
+    let dateA, dateB;
+    if (a.type === "partner") {
+        dateA = new Date(a.date_joined);
+        dateB = new Date(b.date_joined);
+    } else if (a.type === "project") {
+        dateA = new Date(a.datePosted);
+        dateB = new Date(b.datePosted);
+    }
+
+    if (dateA < dateB) {
+        return - 1;
+    } else if (dateA > dateB) {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+const dateCompareDesc = (a, b) => {
+    return -dateCompareAsc(a, b);
+};
+
+const statusCompareAsc = (a, b) => {
+    const statuses = ["Not Started", "In Progress", "Completed"];
+
+    return statuses.indexOf(a.status) - statuses.indexOf(b.status);
+};
+
+const statusCompareDesc = (a, b) => {
+    return - statusCompareAsc(a, b);
 };
 
 export const sortProjectsOptions = [
@@ -84,11 +131,11 @@ export const sortUsersOptions = [
         text: "Name ↓"
     },
     {
-        value: "joined-asc",
+        value: "date-asc",
         text: "Date Joined ↑"
     },
     {
-        value: "joined-desc",
+        value: "date-desc",
         text: "Date Joined ↓"
     }
 ];
@@ -97,11 +144,35 @@ export const SortableList = ({ sortBy, list }) => {
     let sortList;
 
     switch (sortBy) {
-        case 'name-asc':
-            sortList = lst => lst.sort(nameCompareAsc);
+        case "location-asc":
+            sortList = lst => lst.sort(stringCompareAsc("location"));
             break;
-        case 'name-desc':
-            sortList = lst => lst.sort(nameCompareDesc);
+        case "location-desc":
+            sortList = lst => lst.sort(stringCompareDesc("location"));
+            break;
+        case "projects-asc":
+            sortList = lst => lst.sort(numProjectsCompareAsc);
+            break;
+        case "projects-desc":
+            sortList = lst => lst.sort(numProjectsCompareDesc);
+            break;
+        case "name-asc":
+            sortList = lst => lst.sort(stringCompareAsc("name"));
+            break;
+        case "name-desc":
+            sortList = lst => lst.sort(stringCompareDesc("name"));
+            break;
+        case "date-asc":
+            sortList = lst => lst.sort(dateCompareAsc);
+            break;
+        case "date-desc":
+            sortList = lst => lst.sort(dateCompareDesc);
+            break;
+        case "status-asc":
+            sortList = lst => lst.sort(statusCompareAsc);
+            break;
+        case "status-desc":
+            sortList = lst => lst.sort(statusCompareDesc);
             break;
         default:
             sortList = lst => lst.sort((a, b) => a.pk - b.pk);
