4ee57dfc72f92c486e90fec6b87351350645590f
William Wang <wow7@cornell.edu>
2019-07-29 20:34:42 -0400

    got mysql connection working?

--- backend/pryde_backend/settings.py
+++ backend/pryde_backend/settings.py
@@ -200,11 +200,20 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_NAME',
-        'USER': 'DB_USER',
-        'PASSWORD': 'DB_PASSWORD',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'NAME': 'pryde',
+        'USER': 'pryde',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
         'PORT': '3306',
+        'OPTIONS': {
+            # Tell MySQLdb to connect with 'utf8mb4' character set
+            'charset': 'utf8mb4',
+        },
+        # Tell Django to build the test database with the 'utf8mb4' character set
+        'TEST': {
+            'CHARSET': 'utf8mb4',
+            'COLLATION': 'utf8mb4_unicode_ci',
+        }
     }
 }
 
