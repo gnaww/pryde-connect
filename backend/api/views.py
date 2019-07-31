9b66414d06db84cfb3ec22bb70bead9f7ba0f40c
Barron <bfd27@cornell.edu>
2019-07-30 22:08:40 -0400

    wrote search by user... need to figure out who to write one of the filters

--- backend/api/views.py
+++ backend/api/views.py
@@ -180,62 +180,123 @@ class Filter(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
 
         print(request.data)
+        # print(request.data['category'])
+        # x = request.data['category'] != 'projects'
+        # y = request.data['category'] != 'partners'
+        # print(x)
+        # print(y)
+        # print(x and y)
 
+        if 'category' not in request.data or\
+                ((request.data['category'] != 'projects') and (request.data['category'] != 'partners')):
 
+            return Response({'message': 'a category must be specified'})
 
-        filtered_set = Project.objects.all()
+        if request.data['category'] == 'projects':
 
-        if 'q' in request.data:
+            filtered_set = Project.objects.all()
 
-            search_filtered_set = Project.objects.filter(owner__first_name__icontains=request.data['q']) \
-                            | Project.objects.filter(owner__last_name__icontains=request.data['q']) \
-                            | Project.objects.filter(name__icontains=request.data['q']) \
-                            | Project.objects.filter(summary__icontains=request.data['q']) \
-                            | Project.objects.filter(researchTopics__icontains=request.data['q'])
+            if 'q' in request.data:
 
-            filtered_set = filtered_set & search_filtered_set
+                search_filtered_set = Project.objects.filter(owner__first_name__icontains=request.data['q']) \
+                                | Project.objects.filter(owner__last_name__icontains=request.data['q']) \
+                                | Project.objects.filter(name__icontains=request.data['q']) \
+                                | Project.objects.filter(summary__icontains=request.data['q']) \
+                                | Project.objects.filter(researchTopics__icontains=request.data['q'])
 
-        if 'status' in request.data:
-            filter_status_set = Project.objects.none()
-            status_dict = {
-                'Completed': 1,
-                'In Progress': 2,
-                'Not Started': 3
-            }
-            status_params = request.data['status'].split(',')
+                filtered_set = filtered_set & search_filtered_set
 
-            print(status_params)
-            for param in status_params:
-                filter_status_set = filter_status_set | Project.objects.filter(status=status_dict[param])
+            if 'status' in request.data:
+                filter_status_set = Project.objects.none()
+                status_dict = {
+                    'Completed': 1,
+                    'In Progress': 2,
+                    'Not Started': 3
+                }
+                status_params = request.data['status'].split(',')
 
-            filtered_set = filtered_set & filter_status_set
+                print(status_params)
+                for param in status_params:
+                    filter_status_set = filter_status_set | Project.objects.filter(status=status_dict[param])
 
-        if 'researchtopic' in request.data:
-            filtered_researchtopic_set = Project.objects.none()
-            research_topics = request.data['researchtopic'].split(',')
-            for topic in research_topics:
-                filtered_researchtopic_set = filtered_researchtopic_set |\
-                                             Project.objects.filter(researchTopics__contains=[topic])
+                filtered_set = filtered_set & filter_status_set
 
-            filtered_set = filtered_set & filtered_researchtopic_set
+            if 'researchtopic' in request.data:
+                filtered_researchtopic_set = Project.objects.none()
+                research_topics = request.data['researchtopic'].split(',')
+                for topic in research_topics:
+                    filtered_researchtopic_set = filtered_researchtopic_set |\
+                                                 Project.objects.filter(researchTopics__contains=[topic])
 
-        if 'deliverymodes' in request.data:
-            filtered_deliverymodes_set = Project.objects.none()
-            delivery_modes = request.data['deliverymodes'].split(',')
-            for mode in delivery_modes:
-                filtered_deliverymodes_set = filtered_deliverymodes_set |\
-                                             Project.objects.filter(deliveryModes__contains=[mode])
+                filtered_set = filtered_set & filtered_researchtopic_set
 
-            filtered_set = filtered_set & filtered_deliverymodes_set
+            if 'deliverymodes' in request.data:
+                filtered_deliverymodes_set = Project.objects.none()
+                delivery_modes = request.data['deliverymodes'].split(',')
+                for mode in delivery_modes:
+                    filtered_deliverymodes_set = filtered_deliverymodes_set |\
+                                                 Project.objects.filter(deliveryModes__contains=[mode])
 
-        if 'ageranges' in request.data:
-            filtered_ageranges_set = Project.objects.none()
-            ageranges = request.data['ageranges'].split(',')
-            for age in ageranges:
-                filtered_ageranges_set = filtered_ageranges_set | \
-                                         Project.objects.filter(ageRanges__contains=[age])
+                filtered_set = filtered_set & filtered_deliverymodes_set
 
-            filtered_set = filtered_set & filtered_ageranges_set
+            if 'ageranges' in request.data:
+                filtered_ageranges_set = Project.objects.none()
+                ageranges = request.data['ageranges'].split(',')
+                for age in ageranges:
+                    filtered_ageranges_set = filtered_ageranges_set | \
+                                             Project.objects.filter(ageRanges__contains=[age])
 
-        serializer = ProjectShortSerializer(filtered_set, many=True)
-        return Response(data=serializer.data)
+                filtered_set = filtered_set & filtered_ageranges_set
+
+            serializer = ProjectShortSerializer(filtered_set, many=True)
+            return Response(data=serializer.data)
+
+        else:
+            #filter through people
+            filtered_set = PUser.objects.filter(is_staff=False)
+
+            if 'q' in request.data:
+                search_filtered_set = PUser.objects.filter(first_name__icontains=request.data['q']) \
+                                      | PUser.objects.filter(last_name__icontains=request.data['q']) \
+                                      | PUser.objects.filter(researchInterests__icontains=request.data['q']) \
+                                      | PUser.objects.filter(researchDescription__icontains=request.data['q']) \
+                                      | PUser.objects.filter(researchNeeds__icontains=request.data['q']) \
+                #TODO: get users whose projects names contains a query
+
+                filtered_set = filtered_set & search_filtered_set
+
+            # Name, title, research
+            # interests, Research
+            # description, Research
+            # needs(practitioners), & Project
+            # name(s)
+
+            if 'researchinterest' in request.data:
+                filtered_researchinterest_set = PUser.objects.none()
+                research_interests = request.data['researchinterest'].split(',')
+                for n in research_interests:
+                    filtered_researchinterest_set = filtered_researchinterest_set | \
+                                                 PUser.objects.filter(researchInterests__contains=[n])
+
+                filtered_set = filtered_set & filtered_researchinterest_set
+
+            if 'location' in request.data:
+                filtered_location_set = PUser.objects.none()
+                locations = request.data['location'].split(',')
+                for location in locations:
+                    filtered_location_set = filtered_location_set | \
+                                            PUser.objects.filter(location=location)
+
+                filtered_set = filtered_set & filtered_location_set
+
+            if 'ageranges' in request.data:
+                filtered_ageRanges_set = PUser.objects.non()
+                ageRanges = request.data['ageranges'].split(',')
+                for agerange in ageRanges:
+                    filtered_ageRanges_set = filtered_ageRanges_set | \
+                                                PUser.objects.filter(ageRanges__contains=[agerange])
+
+                filtered_set = filtered_set & filtered_ageRanges_set
+
+            serializer = UserShortSerializer(filtered_set, many=True)
+            return Response(data=serializer.data)
