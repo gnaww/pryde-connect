7da879c1f7fbcb60c78de5c8ec80597bb51d7b2b
William Wang <wow7@cornell.edu>
2019-08-06 21:57:32 -0400

    made profile picture optional

--- frontend/src/pages/CreateProfile/UploadProPic.js
+++ frontend/src/pages/CreateProfile/UploadProPic.js
@@ -10,18 +10,12 @@ class UploadProPic extends Component {
             filePreview: null,
             profilePicture: null
         };
-        this.errors = false;
         this.recaptchaRef = React.createRef();
     }
 
     componentDidUpdate(prevProps, _prevState) {
         if (this.props.clickedNext) {
-            this.errors = this.state.profilePicture === null;
-            if (this.state.profilePicture === null) {
-                this.props.onSubmitData(this.state, this.state.profilePicture === null);
-            } else {
-                this.props.onSubmitData(this.state, false);
-            }
+            this.props.onSubmitData(this.state, false);
         }
 
         if (prevProps.savedData !== this.props.savedData) {
@@ -65,7 +59,6 @@ class UploadProPic extends Component {
                 {
                     this.state.filePreview !== null && <img className={styles.uploadedImage} alt={"ERROR"} src={this.state.filePreview} />
                 }
-                {this.errors && <p className={styles.errorMsg}>You must upload a profile picture.</p>}
                 {
                     !this.props.editing &&
                     <ReCAPTCHA
