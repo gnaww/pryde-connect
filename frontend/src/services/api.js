cb2a17fc8dedec6147f7ba3a27819551eed6a1a3
William Wang <wow7@cornell.edu>
2019-08-15 00:12:03 -0400

    add error handling for logout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -111,6 +111,7 @@ export default {
                 }
             })
             .catch(err => {
+                alert("An error occurred while logging out.");
                 console.log(err);
             });
     },
