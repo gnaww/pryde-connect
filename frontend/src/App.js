1fbfd97c5251179da37dba8e5cea05bbb4593bf7
William Wang <wow7@cornell.edu>
2020-03-01 14:31:53 -0500

    revert user warning for too small screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Browse from './pages/Browse';
@@ -21,68 +21,51 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import styles from './App.module.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    componentDidMount() {
-        const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-        if (width < 1410) {
-            alert("Please use this website on a desktop or laptop size screen with the browser fully maximized, or else there may be visual glitches.")
-        }
-    }
-
-    render() {
-        return (
-            <Router>
-                <div className={styles.wrapper}>
-                    <Navbar />
-                    <Switch>
-                        <Route path="/" exact component={Homepage} />
-                        <Route path="/browse" component={Browse} />
-                        <Route path="/login" exact render={renderProps => (
-                            localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <Login {...renderProps} />
-                        )} />
-                        <Route path="/signup" exact render={renderProps => (
-                            localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <CreateProfile {...renderProps} />
-                        )} />
-                        <Route path="/myprofile" exact render={renderProps => (
-                            !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <Profile {...renderProps} />
-                        )} />
-                        <Route path="/submit" exact component={CreateProject} />
-                        <Route path="/user/:id" component={Profile} />
-                        <Route path="/project/:id" component={Project} />
-                        <Route path="/editprofile" exact component={EditProfile} />
-                        <Route path="/editproject" exact component={EditProject} />
-                        <Route path="/success" exact component={SuccessfulDelete} />
-                        <Route path="/password" render={renderProps => (
-                            !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ChangePassword {...renderProps} />
-                        )} />
-                        <Route path="/forgot" render={renderProps => (
-                            localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ForgotPassword {...renderProps} />
-                        )} />
-                        <Route path="/reset/:uid/:token" render={renderProps => (
-                            localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ResetPassword {...renderProps} />
-                        )} />
-                        <Route path="/update" render={renderProps => (
-                            !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <UpdateEmail {...renderProps} />
-                        )} />
-                        <Route path="/preferences" render={renderProps => (
-                            !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <EmailPreferences {...renderProps} />
-                        )} />
-                        <Route path="/confirm/:key" render={renderProps => (
-                            localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <VerifyEmail {...renderProps} />
-                        )} />
-                        <Route component={PageNotFound} />
-                    </Switch>
-                    <Footer />
-                </div>
-            </Router>
-        )
-    }
-}
+const App = () => (
+    <Router>
+        <div className={styles.wrapper}>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Homepage} />
+                <Route path="/browse" component={Browse} />
+                <Route path="/login" exact render={renderProps => (
+                    localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <Login {...renderProps} />
+                )} />
+                <Route path="/signup" exact render={renderProps => (
+                    localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <CreateProfile {...renderProps} />
+                )} />
+                <Route path="/myprofile" exact render={renderProps => (
+                    !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <Profile {...renderProps} />
+                )} />
+                <Route path="/submit" exact component={CreateProject} />
+                <Route path="/user/:id" component={Profile} />
+                <Route path="/project/:id" component={Project} />
+                <Route path="/editprofile" exact component={EditProfile} />
+                <Route path="/editproject" exact component={EditProject} />
+                <Route path="/success" exact component={SuccessfulDelete} />
+                <Route path="/password" render={renderProps => (
+                    !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ChangePassword {...renderProps} />
+                )} />
+                <Route path="/forgot" render={renderProps => (
+                    localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ForgotPassword {...renderProps} />
+                )} />
+                <Route path="/reset/:uid/:token" render={renderProps => (
+                    localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <ResetPassword {...renderProps} />
+                )} />
+                <Route path="/update" render={renderProps => (
+                    !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <UpdateEmail {...renderProps} />
+                )} />
+                <Route path="/preferences" render={renderProps => (
+                    !localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <EmailPreferences {...renderProps} />
+                )} />
+                <Route path="/confirm/:key" render={renderProps => (
+                    localStorage.getItem("pryde_key") ? <Redirect to="/" /> : <VerifyEmail {...renderProps} />
+                )} />
+                <Route component={PageNotFound} />
+            </Switch>
+            <Footer />
+        </div>
+    </Router>
+);
 
 export default App;
